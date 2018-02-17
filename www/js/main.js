c21c55e03187eaa7a3b88c0182511c3544fad4b2
carlosteixeiraa <carlosteixeira.email@gmail.com>
2018-02-17 15:45:31 +0000

    upload ficheiros

---
+++ www/js/main.js
@@ -0,0 +1,107 @@
+var api;
+
+$.getJSON("https://api.myjson.com/bins/lru49", (resposta) => {
+    
+    //console.log(resposta.palavras)
+    api = resposta.palavras;
+    var palavra = random();
+    $('#palavra').text(palavra.palavra);
+    
+
+});
+
+var random = () => {
+
+    var randomp = api[Math.floor(Math.random() * 4)];
+    return randomp;
+};
+
+$("#fechar").on("click", () => {
+    $("#ajuda").fadeOut(300);
+});
+
+$("#jogar").on("click", () => {
+    $("#jogo").fadeIn(100);
+    contador();
+});
+
+$("#bajuda").on("click", () => {
+    //$("#ajuda").css("display", "block"); 
+    $("#ajuda").fadeIn(300);
+});
+
+$("#denovo").on("click", () => {
+    //$("#ajuda").css("display", "block"); 
+    $("#perdeu").fadeOut(200);
+    $("#jogar").css("display", "block"); 
+    segundos = document.getElementById('tempo').innerHTML = 60;
+    clearTimeout(repetir);
+});
+
+
+let segundos;
+
+function contador() {
+
+  segundos = document.getElementById('tempo').innerHTML;
+  $("#jogo").css("background", "#009688"); 
+
+  function mudarPalavra() {
+
+    if(segundos === 10 || segundos === 20 || segundos === 30 || segundos === 40 || segundos === 50) {
+        var palavra = random();
+        $('#palavra').text(palavra.palavra);    
+        $("#jogo").css("background", "#F44336");
+    } else {
+        $("#jogo").css("background", "#009688"); 
+    }
+
+    //console.log(tempo)
+
+    }
+
+    setInterval(mudarPalavra, 1000);
+
+  if (segundos <= 6) {
+
+    function vermelho() {
+        $("#jogo").css("background", "#F44336"); 
+    }
+
+    function azul() {
+        $("#jogo").css("background", "#009688"); 
+    }
+
+    setTimeout(vermelho, 600);
+    setTimeout(azul, 500);
+
+
+    if(segundos <= 0) {
+
+        $("#jogo").css("display", "none");
+        $("#perdeu").css("display", "grid");
+
+
+    }
+
+
+  }
+
+  segundos--;
+
+  tempo = document.getElementById('tempo');
+  tempo.innerHTML = segundos;
+  repetir = setTimeout(contador, 1000);
+} 
+
+// Movimentos ./para depois 
+gyro.startTracking((o) => {
+
+    console.log("x - " + o.x);
+    console.log("y - " + o.y);
+    console.log("z - " + o.z);
+    console.log("alpha - " + o.alpha);
+    console.log("beta - " + o.beta);
+    console.log("gama - " + o.gama);
+
+});
